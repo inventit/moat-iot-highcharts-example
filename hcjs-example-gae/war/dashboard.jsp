47280cd710263bf48c029eb9942004d91753a4fe
Yoshimasa Kawazoe <ykawazoe@yourinventit.com>
2013-05-21 19:15:56 +0900

    add highchart demo code for solor power graph

--- hcjs-example-gae/war/dashboard.jsp
+++ hcjs-example-gae/war/dashboard.jsp
@@ -15,18 +15,6 @@
 		<link rel="icon" type="image/png" href="/moat_icon.png">
 	</head>
 	<body>
-		<%@ page import="java.util.List" %>
-		<%@ page import="java.util.ArrayList" %>
-		<%@ page import="java.util.Map" %>
-		<%@ page import="com.yourinventit.moat.gae.hcjsexample.models.SensingData" %>
-		<%@ page import="com.yourinventit.moat.gae.hcjsexample.models.SysDevice" %>
-		<%@ page import="com.yourinventit.moat.gae.hcjsexample.models.SysDmjob" %>
-		<%
-		final List<SysDevice> devices = (List<SysDevice>) request.getAttribute("devices");
-		final Map<String, List<SensingData>> sensingDataListMap = (Map<String, List<SensingData>>) request.getAttribute("sensing_data_list");
-		final List<SysDmjob> jobList = (List<SysDmjob>) request.getAttribute("job_list");
-		%>
-		
 		<div id="wrap">
 			<div class="container">
 				<h1>
@@ -36,134 +24,9 @@
 				<%-- HighchartsJS --%>
 				<div id="container" style="min-width: 400px; height: 400px; margin: 0 auto"></div>
 
-				<h2>
-					Devices
-				</h2><% if (devices == null || devices.isEmpty()) {%>
-				<p>
-					Install the gateway app and the example Android app into your Android phone. Then tap the <img src="/moat_icon.png" alt="MOAT IoT icon" height="20" width="20"> icon.
-				</p><% } %>
-				<% for (SysDevice d : devices) { %>
-				<table class="table table-striped table-bordered table-condensed">
-					<tr>
-						<th>
-							Name
-						</th>
-						<th>
-							Device ID
-						</th>
-						<th>
-							Commands
-						</th>
-					</tr>
-					<tr>
-						<td>
-							<%= d.getName() %>
-						</td>
-						<td>
-							<%= d.getDeviceId() %>
-						</td>
-						<td>
-							<a href="/dashboard/delete_device?device_uid=<%= d.getUid() %>" class="btn">Delete <i class='icon-off'></i></a>
-						</td>
-					</tr>
-				</table>
-				<h3>
-					Sensing Data for <%= d.getName() %>
-				</h3><%
-				// Building a comma separated string
-				final List<SensingData> list = sensingDataListMap.get(d.getName());
-				%>
-				<table class="table">
-					<tr>
-						<td>
-							<a href="/dashboard/delete_all_sensing_data" class="btn btn-primary">Clear <i class='icon-fire icon-white'></i></a>
-						</td>
-					</tr>
-				</table>
-				<table class="table table-striped table-bordered table-condensed">
-					<tr>
-						<th>
-							Time
-						</th>
-						<th>
-							DC/AC
-						</th>
-						<th>
-							Value
-						</th>
-						<th>
-							Unit
-						</th>
-					</tr>
-					<% for (SensingData sd : list) { %>
-					<tr>
-						<td>
-							<%= sd.getTimestamp() < 1 ? "" : new java.util.Date(sd.getTimestamp()) %>
-						</td>
-						<td>
-							<%= sd.getDa() %>
-						</td>
-						<td>
-							<%= sd.getValue() %>
-						</td>
-						<td>
-							<%= sd.getUnit() %>
-						</td>
-					</tr>
-					<% } %>
-				</table>
-				<hr>
-				<% } // for (SysDevice d : devices) {} %>
-
-				<h2>
-					Active Requests
-				</h2>
-				<table class="table table-striped table-bordered table-condensed">
-					<tr>
-						<th>
-							Device Name
-						</th>
-						<th>
-							Status
-						</th>
-						<th>
-							Type
-						</th>
-						<th>
-							Started At
-						</th>
-						<th>
-							Expired At
-						</th>
-						<th>
-							Action
-						</th>
-					</tr>
-					<% for (SysDmjob j : jobList) { %>
-					<tr>
-						<td>
-							<%= j.getName() %>
-						</td>
-						<td>
-							<%= j.getStatus() %>
-						</td>
-						<td>
-							<%= j.getJobServiceId() %>
-						</td>
-						<td>
-							<%= j.getStartedAt() == null ? "" : j.getStartedAt() %>
-						</td>
-						<td>
-							<%= j.getExpiredAt() == null ? "" : j.getExpiredAt() %>
-						</td>
-						<td>
-							<a href="/dashboard/cancel?uid=<%= j.getUid() %>" class="btn btn-primary">Cancel <i class='icon-remove-sign icon-white'></i></a>
-						</td>
-					</tr>
-					<% } %>
-				</table>
 			<div id="footer">
 				<div class="container">
+				    <br>
 					<p class="muted credit">
 						Copyright &copy; 2013 <a href="http://www.yourinventit.com">Inventit Inc.</a>
 					</p>
--- hcjs-example-gae/war/javascripts/highcharts/dynamic-charts.js
+++ hcjs-example-gae/war/javascripts/highcharts/dynamic-charts.js
@@ -1,80 +1,195 @@
 /*
  * Copyright (C) 2013 InventIt Inc.
- * 
+ *
  * See https://github.com/inventit/moat-iot-highcharts-example
  */
 
-(function($){ // encapsulate jQuery
+function print_added_item(str) {
+    var title = str;
+    var count = 0;
+    return function(unixt, val) {
+        var tt = new Date(unixt);
+        console.log(title + ' added. time: ' + tt.toLocaleString() +
+                    ' value: ' + val + ' total: ' + ++count);
+    }
+}
 
-$(function () {
+(function($) { // encapsulate jQuery
+
+$(function() {
     $(document).ready(function() {
         Highcharts.setOptions({
             global: {
                 useUTC: false
             }
         });
-    
         var chart;
         $('#container').highcharts({
             chart: {
                 type: 'spline',
                 animation: Highcharts.svg, // don't animate in old IE
-                marginRight: 10,
+                //marginRight: 10,
                 events: {
                     load: function() {
-    
-                        // set up the updating of the chart each second
-                        var series = this.series[0];
+                        var series_ampere = this.series[0];
+                        var series_voltage = this.series[1];
+                        var this_chart = this;
                         setInterval(function() {
-                            var x = (new Date()).getTime(), // current time
-                                y = Math.random();
-                            series.addPoint([x, y], true, true);
-                        }, 1000);
+                            var now = new Date();
+                            var tz_offset_msec = now.getTimezoneOffset() * 60 * 1000;
+                            console.log('[' + now.toLocaleString() +
+                                        '] local timezone offset(min): ' +
+                                        now.getTimezoneOffset());
+                            $.get('/sensing_data/query?n=20', function(getdata) {
+                                var addam = print_added_item('ampere');
+                                var addvol = print_added_item('voltage');
+                                var json_str = JSON.stringify(getdata);
+                                console.log('json:' + json_str);
+                                getdata.sort(function(a, b) {
+                                    if (a.timestamp > b.timestamp) {
+                                        return 1;
+                                    } else if (a.timestamp < b.timestamp) {
+                                        return -1;
+                                    } else {
+                                        return 0;
+                                    }
+                                });
+                                console.log('get item(s): ' + getdata.length);
+                                for (var i = 0; i < getdata.length; i++) {
+                                    var is_date_exist = function(seriesarray, timestamp) {
+                                        for (j = 0; j < seriesarray.length; j++) {
+                                            if (seriesarray[j].x == timestamp) {
+                                                return true;
+                                            }
+                                        }
+                                        return false;
+                                    };
+                                    with (getdata[i]) {
+                                        if (unit == 'mA' || unit == 'A') {
+                                            var get_ampere_value = function(type, val) {
+                                                 return (type == 'mA') ? val : val * 1000;
+                                            };
+
+                                            if (!is_date_exist(series_ampere.data, timestamp)) {
+                                                addam(timestamp, value);
+                                                series_ampere.addPoint([timestamp, get_ampere_value(unit, value)], false, true);
+                                            }
+                                        } else if (unit == 'V' || unit == 'mV') {
+                                            var get_voltage_value = function(type, val) {
+                                                 return (type == 'V') ? val : val * 0.001;
+                                            };
+
+                                            if (!is_date_exist(series_voltage.data, timestamp)) {
+                                                addvol(timestamp, value);
+                                                series_voltage.addPoint([timestamp, get_voltage_value(unit, value)], false, true);
+                                            }
+                                        } else {
+                                            console.log('unknown unit: ' + unit);
+                                        }
+                                    }
+                                }
+                                this_chart.redraw();
+                                console.log('@@ ampere item(s) : ' + series_ampere.data.length);
+                                for (j = 0; j < series_ampere.data.length; ++j) {
+                                    var tt = new Date(series_ampere.data[j].x);
+                                    console.log('  time: ' + tt.toLocaleString() +
+                                                ' value: ' + series_ampere.data[j].y);
+                                }
+                                console.log('@@ voltage item(s) : ' + series_voltage.data.length);
+                                for (j = 0; j < series_voltage.data.length; ++j) {
+                                    var tt = new Date(series_voltage.data[j].x);
+                                    console.log('  time: ' + tt.toLocaleString() +
+                                                ' value: ' + series_voltage.data[j].y);
+                                }
+                            });
+                        }, 30000);
                     }
                 }
             },
             title: {
-                text: 'Live random data'
+                text: 'Live solar power data'
             },
             xAxis: {
                 type: 'datetime',
                 tickPixelInterval: 150
             },
-            yAxis: {
+            yAxis: [{
                 title: {
-                    text: 'Value'
+                    text: 'mA'
                 },
-                plotLines: [{
-                    value: 0,
-                    width: 1,
-                    color: '#808080'
-                }]
-            },
+                labels: {
+                    formatter: function() {
+                        return this.value + ' mA';
+                    },
+                    style: {
+                        color: '#808080'
+                    }
+                }
+            }, {
+                title: {
+                    text: 'V'
+                },
+                labels: {
+                    formatter: function() {
+                        return this.value + ' V';
+                    },
+                    style: {
+                        color: '#808080'
+                    }
+                },
+                opposite: true
+            }],
             tooltip: {
                 formatter: function() {
-                        return '<b>'+ this.series.name +'</b><br/>'+
-                        Highcharts.dateFormat('%Y-%m-%d %H:%M:%S', this.x) +'<br/>'+
-                        Highcharts.numberFormat(this.y, 2);
+                        return 'Category: ' + '<b>' + this.series.name + '</b><br/>' +
+                        'Date: ' + Highcharts.dateFormat('%Y-%m-%d %H:%M:%S', this.x) + '<br/>' +
+                        'Value: ' + Highcharts.numberFormat(this.y, 2);
                 }
             },
             legend: {
-                enabled: false
+                layout: 'vertical',
+                align: 'right',
+                verticalAlign: 'top',
+                x: -10,
+                y: 100,
+                borderWidth: 0
             },
             exporting: {
                 enabled: false
             },
             series: [{
-                name: 'Random data',
+                name: 'Ampere',
+                tooltip: {
+                    valueSuffix: 'mA'
+                },
                 data: (function() {
                     // generate an array of random data
                     var data = [],
-                        time = (new Date()).getTime(),
+                        time = (new Date()).getTime() - 100000,
                         i;
-    
-                    for (i = -19; i <= 0; i++) {
+                    for (i = -9; i <= 0; i++) {
                         data.push({
-                            x: time + i * 1000,
-                            y: Math.random()
+                            x: time + i * 10000,
+                            y: 0
+                        });
+                    }
+                    return data;
+                })()
+            }, {
+                name: 'Voltage',
+                yAxis: 1,
+                tooltip: {
+                    valueSuffix: 'V'
+                },
+                data: (function() {
+                    // generate an array of random data
+                    var data = [],
+                        time = (new Date()).getTime() - 100000,
+                        i;
+                    for (i = -9; i <= 0; i++) {
+                        data.push({
+                            x: time + i * 10000,
+                            y: 0
                         });
                     }
                     return data;
@@ -82,6 +197,5 @@ $(function () {
             }]
         });
     });
-    
 });
 })(jQuery);
