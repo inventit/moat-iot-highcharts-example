d7c6209eeb616649b125c3e22810d969f3ddb29c
tyano@yourinventit.com <tyano@yourinventit.com>
2013-06-07 14:56:01 +0900

    remove unnecessary terms

--- hcjs-example-c/pvdemo/src/pvdemo.c
+++ hcjs-example-c/pvdemo/src/pvdemo.c
@@ -1,19 +1,5 @@
 /*
- * LEGAL NOTICE
- *
  * Copyright (C) 2013 InventIt Inc. All rights reserved.
- *
- * This source code, product and/or document is protected under licenses 
- * restricting its use, copying, distribution, and decompilation.
- * No part of this source code, product or document may be reproduced in
- * any form by any means without prior written authorization of InventIt Inc.
- * and its licensors, if any.
- *
- * InventIt Inc.
- * 9F KOJIMACHI CP BUILDING
- * 4-4-7 Kojimachi, Chiyoda-ku, Tokyo 102-0083
- * JAPAN
- * http://www.yourinventit.com/
  */
 
 #include <stdlib.h>
